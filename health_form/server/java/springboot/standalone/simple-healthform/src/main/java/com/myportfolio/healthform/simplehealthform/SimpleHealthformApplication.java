483e2b9f1c2417d79c018d941d55441932f80180
Harold Alcala <harold.dev@gmail.com>
2023-02-19 20:56:40 +0800

    health form - server - springboot

---
+++ health_form/server/java/springboot/standalone/simple-healthform/src/main/java/com/myportfolio/healthform/simplehealthform/SimpleHealthformApplication.java
@@ -0,0 +1,13 @@
+package com.myportfolio.healthform.simplehealthform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleHealthformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleHealthformApplication.class, args);
+	}
+
+}
