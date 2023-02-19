b28b4e287f93bba7626a6400f555a71109cb96be
Harold Alcala <harold.dev@gmail.com>
2023-02-19 17:14:48 +0800

    for static files

--- health_form/server/javascript/nestjs/src/app.module.ts
+++ health_form/server/javascript/nestjs/src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserTempModule } from './user_temp/user_temp.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [UserTempModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserTempModule,
+    ServeStaticModule.forRoot({
+      rootPath: "public",
+    }),],
+  // controllers: [AppController],
+  // providers: [AppService],
 })
 export class AppModule {}
