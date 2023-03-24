27fa3a700a703fe9c355a470f42b9c4aa4231b12
Harold Alcala <harold.dev@gmail.com>
2023-03-25 06:32:41 +0800

    microfrontend

---
+++ microfrontend/home/src/Footer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-xl font-bold">Only the best spinners</div>
+  )
+}
---
+++ microfrontend/home/src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="p-5 bg-blue-500 text-white text-xl font-bold">Fidget Spinner World</div>
+  )
+}
